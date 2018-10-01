c6dba16d9312cef0aeac91b34f16d1f0761515f9
Moataz <moataz.mohammady@gmail.com>
2018-10-01 17:00:56 +0200

    02 - Laravel 5 Tips In Arabic   - #2 Login With Username Or Email Address

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,25 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function field()
+    {
+        return filter_var(request()->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+    }
+
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt([
+            $this->field() => request()->username,
+            'password' => request()->password,
+        ])) {
+            return redirect()->intended('/');
+
+        } else {
+            return redirect()->back()->withInput('username');
+        }
     }
 }
